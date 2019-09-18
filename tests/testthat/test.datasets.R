dceef2141946b3d2729e814aeac303dbebc99923
Tibor V. Varga <tirgit@hotmail.com>
2019-09-18 14:10:40 +0200

    removing german

--- tests/testthat/test.datasets.R
+++ tests/testthat/test.datasets.R
@@ -13,14 +13,14 @@ test_that("number of cols in compas", {
 test_that("number of rows in compas", {
   expect_equal(nrow(compas), 6172)})
 
-test_that("no warning when loading german", {
-  expect_warning(data("german"), NA)})
+#test_that("no warning when loading german", {
+#  expect_warning(data("german"), NA)})
 
-data("german")
-
-test_that("is german a data frame", {
-  expect_true(is.data.frame(german))})
-test_that("number of cols in german", {
-  expect_equal(ncol(german), 23)})
-test_that("number of rows in german", {
-  expect_equal(nrow(german), 1000)})
+#data("german")
+#
+#test_that("is german a data frame", {
+#  expect_true(is.data.frame(german))})
+#test_that("number of cols in german", {
+#  expect_equal(ncol(german), 23)})
+#test_that("number of rows in german", {
+#  expect_equal(nrow(german), 1000)})
